92e54f925a1f55e812042f7c8e5aab5c3089e3b1
Lars399 <235134@student.scalda.nl>
2025-02-12 11:47:06 +0100

    FAQ page working

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,3 +1,64 @@
-<div>
-    <!-- When there is no desire, all things are at peace. - Laozi -->
-</div>
+<x-layout.main>
+
+    <!-- Main Content -->
+    <div class="row triangle-background">
+        <div class="col-sm-2"></div>
+
+        <div class="col-sm-8">
+            <!-- Post 1 -->
+            <div class="backgroundcolorblog faqsection1">
+                <h1 class="textcolorfaqh1">How do you print a document at HZ?</h1>
+                <p class="textcolorfaq">
+                    First, charge your student card. Once it's charged, go to the HZ's
+                    printing website. Follow the instructions, and voilà! You've printed
+                    your document. Well done!
+                </p>
+            </div>
+
+            <!-- Post 2 -->
+            <div class="backgroundcolorblog faqsection2">
+                <h1 class="textcolorfaqh1">How do you scan a document at HZ?</h1>
+                <p class="textcolorfaq">
+                    Scan your student card at the printer, then connect your laptop via
+                    USB. Insert the paper and let the printer do the rest. Great job!
+                </p>
+            </div>
+
+            <!-- Post 3 -->
+            <div class="backgroundcolorblog faqsection3">
+                <h1 class="textcolorfaqh1">How do you use the HZ web shop?</h1>
+                <p class="textcolorfaq">
+                    Go to the HZ web shop and follow the instructions to make your
+                    purchase.
+                </p>
+                <a
+                    href="https://webshop.hz.nl/webshopapp/default.aspx?menu=082076044027019251066025111065201099237062130097"
+                    class="faqlinkcolor"
+                >Link to webshop...</a
+                >
+            </div>
+
+            <!-- Post 4 -->
+            <div class="backgroundcolorblog faqsection4">
+                <h1 class="textcolorfaqh1">How do you book a project space?</h1>
+                <p class="textcolorfaq">
+                    Go to the self-service portal and click "Reserveren." Select
+                    "RESERVEER EEN VERGADER OF PROJECT RUIMTE" and then choose an
+                    available time slot. Fill in the form to confirm your booking.
+                </p>
+            </div>
+
+            <!-- Post 5 -->
+            <div class="backgroundcolorfaq faqsection5">
+                <h1 class="textcolorfaqh1">How do you park at HZ?</h1>
+                <p class="textcolorfaq">
+                    Student parking spaces are limited. You must reserve a spot in
+                    advance.
+                </p>
+            </div>
+        </div>
+
+        <div class="col-sm-2"></div>
+    </div>
+
+</x-layout.main>
