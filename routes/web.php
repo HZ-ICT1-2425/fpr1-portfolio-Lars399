ca83c7b37b13e859dc9c28705bb7292a09125ff3
Lars399 <235134@student.scalda.nl>
2025-02-12 11:45:05 +0100

    Welcome page working
    Added the welcome page + layout blade file. Made preps for the other pages.
    for now i use inline styling in the layout blade file. THIS WILL CHANGE! I don't know how yet
    
    There will come more pushes with the other pages...

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('/portfolio', function () {
+    return view('portfolio');
+})->name('portfolio');
+
+Route::get('/faq', function () {
+    return view('faq');
+})->name('faq');
